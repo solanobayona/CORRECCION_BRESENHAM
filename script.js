a9c0871782d540448960aab5ffb90de7c91ecce6
solanobayona <est.cristian.solano@unimilitar.edu.co>
2026-04-15 13:09:20 -0500

    Se añade el algoritmo de Bresenham.

--- script.js
+++ script.js
@@ -3,3 +3,28 @@ const ctx = canvas.getContext("2d");
 function plot(x, y) {
     ctx.fillRect(x, canvas.height - y, 1, 1);
 }
+function bresenham(x0, y0, x1, y1, plot) {
+    let dx = Math.abs(x1 - x0);
+    let dy = Math.abs(y1 - y0);
+    let sx = (x0 < x1) ? 1 : -1;
+    let sy = (y0 < y1) ? 1 : -1;
+    let err = dx - dy;
+
+    while (true) {
+        plot(x0, y0);
+
+        if (x0 === x1 && y0 === y1) break;
+
+        let e2 = 2 * err;
+
+        if (e2 > -dy) {
+            err -= dy;
+            x0 += sx;
+        }
+
+        if (e2 < dx) {
+            err += dx;
+            y0 += sy;
+        }
+    }
+}
