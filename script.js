a0652e064d5fbc5641f5f079b2c64d61420403c9
solanobayona <est.cristian.solano@unimilitar.edu.co>
2026-04-15 13:12:09 -0500

    Se crea la función principal de dibujo.

--- script.js
+++ script.js
@@ -28,3 +28,13 @@ function bresenham(x0, y0, x1, y1, plot) {
         }
     }
 }
+function dibujar() {
+    let x0 = parseInt(document.getElementById("x0").value);
+    let y0 = parseInt(document.getElementById("y0").value);
+    let x1 = parseInt(document.getElementById("x1").value);
+    let y1 = parseInt(document.getElementById("y1").value);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    bresenham(x0, y0, x1, y1, plot);
+}
